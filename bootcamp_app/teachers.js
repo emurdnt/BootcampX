3acea5bf64bb8be42a8483c13d5628b1ff580901
Em Urdaneta <em.urdaneta94@gmail.com>
2020-08-18 23:25:33 +0000

    set up node Postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM assistance_requests
+  JOIN teachers on assistance_requests.teacher_id = teachers.id 
+  JOIN students on assistance_requests.student_id = students.id
+  JOIN cohorts on students.cohort_id = cohorts.id
+  WHERE cohorts.name = '${process.argv[2]}'
+  ORDER BY teacher;
+`)
+.then(res => {
+  // console.log(res.rows);
+  res.rows.forEach(data => {
+    console.log(`${data.cohort} : ${data.teacher}`);
+  })
+}).catch(err => console.error('query error', err.stack));
