2b51b4adbbf8563b39329863fac681b409f28584
Em Urdaneta <em.urdaneta94@gmail.com>
2020-08-18 23:32:29 +0000

    modify project to avoid sql injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-  FROM assistance_requests
-  JOIN teachers on assistance_requests.teacher_id = teachers.id 
-  JOIN students on assistance_requests.student_id = students.id
-  JOIN cohorts on students.cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2]}'
-  ORDER BY teacher;
-`)
+const cohortName = process.argv[2];
+const values = [cohortName];
+const queryString = `
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers on assistance_requests.teacher_id = teachers.id 
+JOIN students on assistance_requests.student_id = students.id
+JOIN cohorts on students.cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+  `;
+
+pool.query(queryString,values)
 .then(res => {
   // console.log(res.rows);
   res.rows.forEach(data => {
